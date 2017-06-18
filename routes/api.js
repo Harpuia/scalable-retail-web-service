edd8e9ea662de47b093dae62e02a394f0fd8babf
Yuchao Zhou <yuchao.y.zhou@gmail.com>
2017-06-18 18:07:01 -0400

    RegisterUser implemented and tested

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,54 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 var mysql = require('mysql');
+var model = require('../models/model');
 var sess;
 
+router.post("/registerUser", jsonParser, function (req, res) {
+    var fname = req.body.fname;
+    var lname = req.body.lname;
+    var address = req.body.address;
+    var city = req.body.city;
+    var state = req.body.state;
+    var zip = req.body.zip;
+    var email = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
 
+    var failureRes = {
+        message: "The input you provided is not valid"
+    };
+
+    if (!(fname && lname && address && city && state && zip && email && username && password)) {
+        res.json(failureRes);
+    }
+
+    model.localConnection.getConnection(function(err, conn) {
+        if(err) throw err;
+        var sql = "SELECT * FROM users WHERE username = " + mysql.escape(username);
+        conn.query(sql, function(err, result) {
+            if(err) throw err;
+            
+            if(typeof result !== 'undefined' && result.length > 0) {
+                res.json(failureRes);
+            } else {
+                sql = "INSERT INTO `users` (`fname`, `lname`, `address`, `city`, `state`, `zip`, `email`, `username`, `password`, `role`) VALUES (" + 
+                    mysql.escape(fname) + ", " + mysql.escape(lname) + ", " + mysql.escape(address) + ", " + mysql.escape(city) + ", " + 
+                    mysql.escape(state) + ", " + mysql.escape(zip) + ", " + mysql.escape(email) + ", " + mysql.escape(username) + ", " +
+                    mysql.escape(password) + ", " + mysql.escape("customer") + ")";
+                conn.query(sql, function(err, result) {
+                    conn.release();
+                    if(err) throw err;
+                    var successMessage = fname + " was registered successfully";
+                    res.json({
+                        message: successMessage
+                    });
+                });
+            } 
+        })
+        
+    })
+});
 
 router.post("/login", jsonParser, function (req, res) {
     var username = req.body.username;
@@ -16,25 +61,14 @@ router.post("/login", jsonParser, function (req, res) {
         message: "There seems to be an issue with the username/password combination that you entered"
     };
 
-    var con = mysql.createConnection({
-        // Uncomment the following host if you've deployed the MySQL server locally.
-        // host: "localhost",
-
-        // When using MySQL instance deployed on AWS RDS service.
-        host: "simpleamazon.cektrjgecscm.us-east-1.rds.amazonaws.com",
-        user: "root",
-        password: "ediss_is_awesome",
-        database: "e_commerce"
-    });
-
-    con.connect(function(err) {
+    model.localConnection.getConnection(function(err, conn) {
         if (err) throw err;
         var sql = "SELECT * FROM users WHERE username = " + mysql.escape(username) + " AND password = " + mysql.escape(password);
-        con.query(sql, function (err, result) {
+        conn.query(sql, function (err, result) {
             if (err) throw err;
             
             if (typeof result !== 'undefined' && result.length > 0) {
-                var welcomeMessage = "Welcome " + result[0].firstname;
+                var welcomeMessage = "Welcome " + result[0].fname;
                 res.json ({
                     message: welcomeMessage
                 });
