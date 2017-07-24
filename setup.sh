80de2633674971cdcb5e7bc21ac7de62ee0abde7
Yuchao Zhou <yuchao.y.zhou@gmail.com>
2017-07-24 14:55:30 -0400

    Reorder the test data and script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get -y update
 sudo apt-get install -y nodejs
 sudo apt-get install -y git
 sudo apt-get install -y npm
 sudo apt-get install redis-server
-redis-server &
 git clone https://Harpuia@github.com/Harpuia/SimpleAmazon.git
 cd SimpleAmazon
 npm install
